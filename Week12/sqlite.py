fe5616530d62bc12bafa978bf911797333c48246
Aisha <aishaabbas151004@gmail.com>
2026-06-22 10:52:48 +0500

    Completed SQLite task

--- Week12/sqlite.py
+++ Week12/sqlite.py
@@ -1,14 +1,21 @@
 import sqlite3
 
+con = sqlite3.connect("project.db")
+cursor = con.cursor()
 
-con=sqlite3.connect("project.db")
-cursor=con.cursor()
-
-cursor.execute()  # add create table query
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER,
+    department TEXT
+)
+""")
 
 con.commit()
 con.close()
 
+print("Table created successfully!")
 
 
 
